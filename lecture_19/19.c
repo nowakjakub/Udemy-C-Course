ab9b7277527c31924905d29c2934d9f964f415b1
nowakjakub <novik21e@gmail.com>
2017-09-30 11:38:49 +0200

    Introduction to printf

---
+++ lecture_19/19.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Hello World\n");
+    puts("Hello world ponownie\n");
+
+    printf("There are %d bottles standing on the %s\n", 20, "wall");
+
+    return 0;
+}
