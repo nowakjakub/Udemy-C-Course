517959bfead621a3d72679bfce262982c962096c
nowakjakub <novik21e@gmail.com>
2018-02-21 18:40:29 +0100

    Lecture 39 - Arrays

---
+++ lecture_39/39.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int intarray[5];
+
+int main(int argc, char **argv){
+	int i;
+
+	intarray[0] = 1;
+	intarray[1] = 11;
+	intarray[2] = 21;
+	intarray[3] = 31;
+	intarray[4] = 41;
+
+	for(i = 0; i < 5; ++i){
+		printf("%d\n",intarray[i]);
+	}
+	return 0;
+}
